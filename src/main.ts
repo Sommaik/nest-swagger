f18bc6a6a3ab4faf272ae62a110d0c366442f54d
sommaik <sommai.k@gmail.com>
2022-08-05 03:38:33 +0000

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    // .setBasePath('/simple') // for v.2
+    .addServer('http://localhost:3232')
+    .addServer('http://localhost:8000/simple')
+    .addServer('http://192.168.1.101:8000/simple')
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3232);
 }
 bootstrap();
